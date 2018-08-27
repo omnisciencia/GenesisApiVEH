83bb746f39822fc0fc84ed1b259b7834c610cb93
JARA <JARA@DESKTOP-JARA>
2018-08-27 16:41:37 -0500

    jjara

--- Infraestrutura.Data.SqlServer/General_DAL.cs
+++ Infraestrutura.Data.SqlServer/General_DAL.cs
@@ -795,6 +795,7 @@ namespace Infraestrutura.Data.SqlServer
                 clase.finvigencia = dr["finvigencia"].ToString();
                 clase.tippoliza = int.Parse(dr["tippoliza"].ToString());
                 clase.planproducto = dr["planproducto"].ToString();
+                
 
                 listado.Add(clase);
             }
