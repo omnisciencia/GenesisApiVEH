89ccbd011d1ad5cca0d76f8c6073a62bced1e491
Cristopher_OSC <Cristopher_OSC@CRISTOPHER_PC>
2018-09-06 11:02:54 -0500

    ctorres060918

---
+++ QGenesisVEH/Controllers/GestionSiniestroController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QGenesisVEH.Controllers
+{
+    public class GestionSiniestroController : Controller
+    {
+        // GET: GestionSiniestro
+        public ActionResult RegistrarSiniestro()
+        {
+            return View();
+        }
+    }
+}
